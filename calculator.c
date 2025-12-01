d9ae93abf58042d3c418a97c36941bbbbe51fe87
krtikyy <kartikey1796@gmail.com>
2025-12-01 22:03:42 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int a,b,r;
+    char op;
+    
+    printf("Enter the equation : ");
+    scanf("%d %c %d",&a,&op,&b);
+
+    switch(op)
+    {
+        case '+' :
+        r=a+b;
+        break;
+        case '*' :
+        r=a*b;
+        case '-' :
+        r=a-b;
+        break;
+        case '/' :
+        r=a/b;
+        break;
+        default :
+        printf("Invalid equation");
+        exit(1);
+    }
+    printf("Result = %d",r);
+
+}
