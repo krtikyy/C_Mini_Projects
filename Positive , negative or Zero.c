dafa6323022f9fe17d87c86cbf18f7a74827c705
Kartikeyy <kartikey1796@gmail.com>
2025-12-02 14:44:23 +0530

    Add files via upload

---
+++ Positive , negative or Zero.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main() 
+{
+    float a;
+    
+printf("Enter a Number : ");
+scanf("%f",&a);
+
+if (a>0)
+{
+    printf("Positive");
+
+}
+else if (a<0)
+{
+    printf("Negative");
+}
+else
+{
+    printf("Zero") ;
+}
+
+
+
+
+}
