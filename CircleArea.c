61aac989a7ab0cddbdabb02d22d53024e914657f
Kartikeyy <kartikey1796@gmail.com>
2025-12-02 15:15:28 +0530

    Add files via upload

---
+++ CircleArea.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	
+	float p = 3.14 , r , area;
+	printf("Enter the radius of circle(in cm): ");
+	scanf("%f",&r);
+	area = p*r*r ;
+	printf("Area of Circle is : %fcm^2" , area);
+	
+	
+	
+	
+}
